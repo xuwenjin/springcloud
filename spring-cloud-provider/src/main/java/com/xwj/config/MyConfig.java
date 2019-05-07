5bdefceab714084c9aeb3a4f40984e9921894ef7
xuwenjin <563210736@qq.com>
2019-05-07 16:57:20 +0800

    增加配置文件加密解密功能-jasypt

---
+++ spring-cloud-provider/src/main/java/com/xwj/config/MyConfig.java
@@ -0,0 +1,34 @@
+package com.xwj.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类
+ */
+@Configuration
+public class MyConfig {
+
+	/**
+	 * 配置StringEncryptor
+	 */
+	@Bean("jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword("EbfYkitulv73I2p0mXI50JMXoaxZTKJ7");
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setProviderName("SunJCE");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setIvGeneratorClassName("org.jasypt.salt.NoOpIVGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+		return encryptor;
+	}
+
+}
--- spring-cloud-provider/src/main/java/com/xwj/controller/IndexController.java
+++ spring-cloud-provider/src/main/java/com/xwj/controller/IndexController.java
@@ -1,16 +1,54 @@
 package com.xwj.controller;
 
+import javax.annotation.Resource;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.xwj.operlog.MyLog;
 
 @Controller
 public class IndexController {
 
+	@Resource(name = "jasyptStringEncryptor")
+	private StringEncryptor encryptor;
+	
+	@Value("${spring.datasource.password}")
+	private String password;
+
 	@MyLog("登录")
 	@GetMapping("/login")
 	public void login() {
+	}
+
+	/**
+	 * 测试加密
+	 */
+	@ResponseBody
+	@GetMapping("/encrypt/{str}")
+	public String testEncrypt(@PathVariable String str) {
+		return encryptor.encrypt(str);
+	}
+
+	/**
+	 * 测试解密
+	 * 
+	 * @param encryptStr 加密后的字符串
+	 */
+	@ResponseBody
+	@GetMapping("/decrypt/{encryptStr}")
+	public String testDecrypt(@PathVariable String encryptStr) {
+		return encryptor.decrypt(encryptStr);
+	}
+	
+	@ResponseBody
+	@GetMapping("/password")
+	public String password() {
+		return password;
 	}
 
 }
