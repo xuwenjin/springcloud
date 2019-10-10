f81ffdb565875886da88e17bb8601bc6912d4a24
xuwenjin <563210736@qq.com>
2019-10-10 17:58:33 +0800

    修改手写redis分布式锁注释

--- spring-cloud-provider/src/main/java/com/xwj/lock/RedisLock.java
+++ spring-cloud-provider/src/main/java/com/xwj/lock/RedisLock.java
@@ -4,10 +4,10 @@ import java.util.List;
 import java.util.UUID;
 
 import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import lombok.extern.slf4j.Slf4j;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.Transaction;
@@ -18,20 +18,19 @@ import redis.clients.jedis.exceptions.JedisException;
  * 
  * @author xwj
  */
-@Slf4j
 @Component
 public class RedisLock {
 
 	private static final String LOCK_SUCCESS = ObjectUtils.CONST("OK");
 	private static final String SET_IF_NOT_EXIST = ObjectUtils.CONST("NX");
 	private static final String SET_WITH_EXPIRE_TIME = ObjectUtils.CONST("PX");
-	private static final long ACQUIRE_TIMEOUT = ObjectUtils.CONST(10000);
-	private static final long TIMEOUT = ObjectUtils.CONST(500);
+	private static final long ACQUIRE_TIMEOUT = ObjectUtils.CONST(10000); // 等待获取锁的时间
+	private static final long TIMEOUT = ObjectUtils.CONST(500); // 获取锁后过期时间
 
 	@Autowired
 	private JedisPool jedisPool;
 
-	private final static ThreadLocal<String> idStore = new ThreadLocal<String>();
+	private final static ThreadLocal<String> ID_STORE = new ThreadLocal<String>();
 
 	/**
 	 * 加锁
@@ -45,9 +44,7 @@ public class RedisLock {
 			System.out.println("获取连接池时间:" + (System.currentTimeMillis() - start1));
 			// 随机生成一个id，用于解锁
 			String identifier = UUID.randomUUID().toString();
-			// 记录开始时间
-			long start = System.currentTimeMillis();
-			// 获取锁的超时时间，上锁后超过此时间则自动释放锁
+			// 尝试获取锁的时间，超时返回false
 			long end = System.currentTimeMillis() + ACQUIRE_TIMEOUT;
 			while (System.currentTimeMillis() < end) {
 				/*
@@ -56,11 +53,9 @@ public class RedisLock {
 				 */
 				String result = jedis.set(lockKey, identifier, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, TIMEOUT);
 				if (LOCK_SUCCESS.equals(result)) {
-					idStore.set(identifier);
-					log.info("lockTime:" + (System.currentTimeMillis() - start));
+					ID_STORE.set(identifier);
 					return true;
 				}
-//				System.out.println("waiting...");
 				try {
 					Thread.sleep(20);
 				} catch (InterruptedException e) {
@@ -91,8 +86,7 @@ public class RedisLock {
 				// 如果有至少一个被监视的键在 EXEC执行之前被修改了，那么整个事务都会被取消
 				jedis.watch(lockKey);
 				// 通过前面返回的value值判断是不是该锁，若是该锁，则删除，释放锁
-				if (idStore.get().equals(jedis.get(lockKey))) {
-					System.out.println("解锁...");
+				if (StringUtils.equals(ID_STORE.get(), jedis.get(lockKey))) {
 					// 开启事物
 					Transaction transaction = jedis.multi();
 					transaction.del(lockKey);
@@ -113,7 +107,7 @@ public class RedisLock {
 		} finally {
 			if (jedis != null) {
 				jedis.close();
-				idStore.remove();
+				ID_STORE.remove();
 			}
 		}
 		return retFlag;
