34e7a288e2118aefb2f1a0d7d633f31e0bf573a2
xuwenjin <563210736@qq.com>
2020-04-09 08:47:59 +0800

    修改注释

--- spring-cloud-websocket/src/main/java/com/xwj/config/WebSocketConfig.java
+++ spring-cloud-websocket/src/main/java/com/xwj/config/WebSocketConfig.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
- * 使用此配置可以关闭servlet容器对websocket端点的扫描
+ * WebSocket配置
  */
 @Configuration
 public class WebSocketConfig {
 
+	/**
+	 * 注入一个ServerEndpointExporter,该Bean会自动注册使用@ServerEndpoint注解申明的websocket endpoint
+	 */
 	@Bean
 	public ServerEndpointExporter serverEndpointExporter() {
 		return new ServerEndpointExporter();
