2b863227a32a6833a157439e8ad7188fb099d944
xuwenjin <563210736@qq.com>
2020-08-21 14:26:50 +0800

    测试redis的list数据类型

--- spring-cloud-provider/src/main/java/com/xwj/controller/RedisController.java
+++ spring-cloud-provider/src/main/java/com/xwj/controller/RedisController.java
@@ -1,5 +1,6 @@
 package com.xwj.controller;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
@@ -8,6 +9,7 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.integration.redis.util.RedisLockRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -220,6 +222,144 @@ public class RedisController implements InitializingBean {
 		Long max = System.currentTimeMillis();
 		redisTemplate.opsForZSet().add(key, 7, System.currentTimeMillis());
 		System.out.println("指定时间范围内数据：" + redisTemplate.opsForZSet().reverseRangeByScore(key, min, max));
+	}
+
+	/**
+	 * 测试list类型-left
+	 */
+	@GetMapping("/listLeft")
+	public void listLeft() {
+		String key = "listLeft";
+		ListOperations<String, Object> operations = redisTemplate.opsForList();
+
+		// 变量左边添加元素值
+		// operations.leftPush(key, "a");
+		// operations.leftPush(key, "b");
+		// operations.leftPush(key, "c");
+		// operations.leftPush(key, "d");
+		// 数据顺序： d c b a
+		// 数据下标： 0 1 2 3
+		// 最后一位是-1
+
+		// 获取集合指定位置的值()
+		System.out.println(operations.index(key, 1));
+		System.out.println(operations.index(key, -1));
+
+		// 获取指定区间的值
+		System.out.println(operations.range(key, 1, 3));
+		System.out.println(operations.range(key, 1, -2));
+
+		// 变量左边批量添加元素值
+		// operations.leftPushAll(key, "d", "e");
+		// System.out.println(operations.range(key, 0, -1));
+		// operations.leftPushAll(key, Arrays.asList("f", "g"));
+		// System.out.println(operations.range(key, 0, -1));
+
+		// 如果key存在，则添加元素，否则不添加
+		// operations.leftPushIfPresent(key, "e");
+		// System.out.println(operations.range(key, 0, -1));
+		// operations.leftPushIfPresent(key + "1", "haha");
+		// System.out.println(operations.range(key, 0, -1));
+	}
+
+	/**
+	 * 测试list类型-right
+	 */
+	@GetMapping("/listRight")
+	public void listRight() {
+		String key = "listRight";
+		ListOperations<String, Object> operations = redisTemplate.opsForList();
+
+		// 变量左边添加元素值
+		// operations.rightPush(key, "a");
+		// operations.rightPush(key, "b");
+		// operations.rightPush(key, "c");
+		// operations.rightPush(key, "d");
+		// 数据顺序： a b c d
+		// 数据下标： 0 1 2 3
+		// 最后一位是-1
+
+		// 获取集合指定位置的值()
+		System.out.println(operations.index(key, 1));
+		System.out.println(operations.index(key, -1));
+
+		// 获取指定区间的值
+		System.out.println(operations.range(key, 1, 3));
+		System.out.println(operations.range(key, 1, -2));
+
+		// 变量右边批量添加元素值
+		operations.rightPushAll(key, "d", "e");
+		System.out.println(operations.range(key, 0, -1));
+		operations.rightPushAll(key, Arrays.asList("f", "g"));
+		System.out.println(operations.range(key, 0, -1));
+
+		// 如果存在集合则添加元素
+		operations.rightPushIfPresent(key, "e");
+		System.out.println(operations.range(key, 0, -1));
+		operations.rightPushIfPresent(key, "haha");
+		System.out.println(operations.range(key, 0, -1));
+	}
+
+	/**
+	 * 测试list类型
+	 */
+	@GetMapping("/list")
+	public void list() {
+		String key = "list";
+		ListOperations<String, Object> operations = redisTemplate.opsForList();
+
+		// 变量左边添加元素值
+		operations.leftPush(key, "a");
+		operations.leftPush(key, "b");
+		operations.leftPush(key, "c");
+		operations.rightPush(key, "d");
+		operations.rightPush(key, "e");
+		operations.rightPush(key, "f");
+		// 数据顺序： c b a d e f
+		// 数据下标： 0 1 2 3 4 5
+		// 最后一位是-1
+
+		// 获取集合长度
+		System.out.println(operations.size(key));
+
+		// 移除集合左边第一个元素
+		// operations.leftPop(key);
+		// System.out.println(operations.range(key, 0, -1));
+
+		// 在等待的时间里，移除集合中左边的元素，如果超过等待的时间仍没有元素则退出
+		// operations.leftPop(key, 1, TimeUnit.SECONDS);
+		// System.out.println(operations.range(key, 0, -1));
+
+		// 移除集合右边第一个元素
+		// operations.rightPop(key);
+		// System.out.println(operations.range(key, 0, -1));
+
+		// 在等待的时间里，移除集合中右边的元素，如果超过等待的时间仍没有元素则退出
+		// operations.rightPop(key, 1, TimeUnit.SECONDS);
+		// System.out.println(operations.range(key, 0, -1));
+
+		// 移除sourceKey中右边的元素，同时将移除的元素添加在destinationKey左边 ------先进先出(可以做消息队列)
+		operations.rightPopAndLeftPush(key, key);
+		System.out.println(operations.range(key, 0, -1));
+
+		// 在等待的时间里，移除sourceKey中右边的元素，同时将移除的元素添加在destinationKey左边，如果超过等待的时间仍没有元素则退出
+		// ------先进先出(可以做消息队列)
+		operations.rightPopAndLeftPush(key, "newList", 1, TimeUnit.SECONDS);
+		System.out.println("移除后剩余元素：" + operations.range(key, 0, -1));
+		System.out.println("添加后元素：" + operations.range("newList", 0, -1));
+
+		// 在集合的指定位置插入元素,如果指定位置已有元素，则覆盖，没有则新增，超过集合下标+n则会报错
+		operations.set(key, 3, "set");
+		System.out.println(operations.range(key, 0, -1));
+
+		// 从存储在键中的列表中删除等于值的元素的第一个计数事件。count> 0：删除等于从左到右移动的值的第一个元素；count<
+		// 0：删除等于从右到左移动的值的第一个元素；count = 0：删除等于value的所有元素。
+		operations.remove(key, 0, "b");
+		System.out.println(operations.range(key, 0, -1));
+
+		// 截取集合元素长度，保留长度内的数据
+		operations.trim(key, 0, 2);
+		System.out.println("截取后剩余元素：" + operations.range(key, 0, -1));
 	}
 
 }
