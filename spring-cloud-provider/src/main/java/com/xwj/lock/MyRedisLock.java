a535a514a583ff6ba55617d2f4f7ae45f3f691e0
xuwenjin <563210736@qq.com>
2020-12-30 14:46:01 +0800

    修改可重入锁实现

--- spring-cloud-provider/src/main/java/com/xwj/lock/MyRedisLock.java
+++ spring-cloud-provider/src/main/java/com/xwj/lock/MyRedisLock.java
@@ -16,9 +16,14 @@ import com.xwj.redis.JsonRedisTemplate;
  */
 public class MyRedisLock {
 
+	/**获取锁后，过期时间*/
 	protected long lockLeaseTime;
+
+	/**传进来的锁名称*/
 	private String name;
-	final UUID id;
+
+	/**uuid*/
+	private final UUID id;
 
 	private JsonRedisTemplate redisTemplate;
 
@@ -41,7 +46,7 @@ public class MyRedisLock {
 		long time = unit.toMillis(waitTime);
 		long threadId = Thread.currentThread().getId();
 		Long ttl = tryAcquire(leaseTime, TimeUnit.MILLISECONDS, threadId);
-//		System.out.println("ttl------>" + ttl);
+		// System.out.println("ttl------>" + ttl);
 		if (ttl == null) {
 			// 加锁成功
 			return true;
@@ -72,13 +77,6 @@ public class MyRedisLock {
 	}
 
 	private Long tryAcquire(long leaseTime, TimeUnit unit, long threadId) {
-		if (leaseTime != -1) {
-			return tryLockInnerAsync(leaseTime, unit, threadId);
-		}
-		return null;
-	}
-
-	private Long tryLockInnerAsync(long leaseTime, TimeUnit unit, long threadId) {
 		lockLeaseTime = unit.toMillis(leaseTime);
 
 		String luaScript = buildLockLuaScript();
@@ -86,10 +84,44 @@ public class MyRedisLock {
 
 		ImmutableList<String> keys = ImmutableList.of(name);
 		String lockName = getLockName(threadId);
-		
-//		System.out.println("lock -------->  key:" + name +", hkey:" + lockName +", leaseTime:" + lockLeaseTime);
-		
+
+		// System.out.println("lock --------> key:" + name +", hkey:" + lockName
+		// +", leaseTime:" + lockLeaseTime);
+
 		return redisTemplate.execute(redisScript, keys, leaseTime, lockName);
+	}
+
+	/**
+	 * 解锁
+	 */
+	public void unlock() {
+		long threadId = Thread.currentThread().getId();
+
+		String luaScript = buildUnLockLuaScript();
+		RedisScript<Long> redisScript = new DefaultRedisScript<>(luaScript, Long.class);
+
+		ImmutableList<String> keys = ImmutableList.of(name, getChannelName());
+		String lockName = getLockName(threadId);
+
+		Long res = redisTemplate.execute(redisScript, keys, UNLOCK_MESSAGE, lockLeaseTime, lockName);
+
+		System.out.println("unlock.res ----->" + res);
+	}
+
+	/**
+	 * 拼锁的key。如：8743c9c0-0795-4907-87fd-6c719a6b4586:1
+	 * 
+	 * @param threadId 线程id
+	 */
+	protected String getLockName(long threadId) {
+		return id + ":" + threadId;
+	}
+
+	/**
+	 * 加个前缀
+	 */
+	private String getChannelName() {
+		return "redisson_lock__channel" + ":{" + name + "}";
 	}
 
 	/**
@@ -105,60 +137,28 @@ public class MyRedisLock {
 		 * 
 		 * ARGV[2] = HK   ---> uuid + ":" threadId
 		 */
-		return 
-			// 首先用的exists 判断redis中是否存在当前key，如果不存在就等于0，然后执行hset指令，将“anyLock id:threadId 1”存储到redis中。
-			// 最终redis存储的数据类似于："8743c9c0-0795-4907-87fd-6c719a6b4586:1":1
-			// 最后面的一个1 是为了后面可重入做的计数统计
-			"if (redis.call('exists', KEYS[1]) == 0) then " +      
-	 			"redis.call('hset', KEYS[1], ARGV[2], 1); " +       
-	 			"redis.call('pexpire', KEYS[1], ARGV[1]); " + 
-	 			//返回空值 nil，获取锁成功
-	 			"return nil; " + 
-	 		"end; "
-	 		// 如果同一个机器同一个线程再次来请求，这里就会执行hincrby，将value+1变成了2，然后继续设置过期时间。
-			+ "if (redis.call('hexists', KEYS[1], ARGV[2]) == 1) then " + 
-	 			"redis.call('hincrby', KEYS[1], ARGV[2], 1); " + 
-	 			"redis.call('pexpire', KEYS[1], ARGV[1]); " + 
-	 			//返回空值nil，重入获取锁成功
-	 			"return nil; " + 
-	 		"end; "
-	 		// 返回锁的存活时间(表明获取锁失败)
-			+ "return redis.call('pttl', KEYS[1]);";
+		return
+		// 首先用的exists 判断redis中是否存在当前key，如果不存在就等于0，然后执行hset指令，将“anyLock
+		// id:threadId 1”存储到redis中。
+		// 最终redis存储的数据类似于："8743c9c0-0795-4907-87fd-6c719a6b4586:1":1
+		// 最后面的一个1 是为了后面可重入做的计数统计
+		"if (redis.call('exists', KEYS[1]) == 0) then " + 
+			"redis.call('hset', KEYS[1], ARGV[2], 1); " + 
+			"redis.call('pexpire', KEYS[1], ARGV[1]); " +
+			// 返回空值 nil，获取锁成功
+			"return nil; " + 
+		"end; " +
+		// 如果同一个机器同一个线程再次来请求，这里就会执行hincrby，将value+1变成了2，然后继续设置过期时间。
+		"if (redis.call('hexists', KEYS[1], ARGV[2]) == 1) then " + 
+			"redis.call('hincrby', KEYS[1], ARGV[2], 1); " + 
+			"redis.call('pexpire', KEYS[1], ARGV[1]); " +
+			// 返回空值nil，重入获取锁成功
+			"return nil; " + 
+		"end; " + 
+			// 返回锁的存活时间(表明获取锁失败)
+			"return redis.call('pttl', KEYS[1]);";
 	}
 
-	/**
-	 * 解锁
-	 */
-	public void unlock() {
-		long threadId = Thread.currentThread().getId();
-		unlockAsync(threadId);
-	}
-	
-	public void unlockAsync(long threadId) {
-		String luaScript = buildUnLockLuaScript();
-		RedisScript<Long> redisScript = new DefaultRedisScript<>(luaScript, Long.class);
-
-		ImmutableList<String> keys = ImmutableList.of(name, getChannelName());
-		String lockName = getLockName(threadId);
-		
-//		System.out.println("unlock -------->  key:" + keys +", hkey:" + lockName +", leaseTime:" + lockLeaseTime);
-		
-		Long res = redisTemplate.execute(redisScript, keys, UNLOCK_MESSAGE, lockLeaseTime, lockName);
-		
-		System.out.println("unlock.res ----->" + res);
-	}
-	
-	private String getChannelName() {
-        return prefixName("redisson_lock__channel", name);
-    }
-    
-    private static String prefixName(String prefix, String name) {
-        if (name.contains("{")) {
-            return prefix + ":" + name;
-        }
-        return prefix + ":{" + name + "}";
-    }
-	
 	/**
 	 * lua解锁脚本
 	 * 
@@ -176,28 +176,24 @@ public class MyRedisLock {
 		 * 
 		 * ARGV[3] = HK   ---> uuid + ":" threadId
 		 */
-		return 
-			// 判断当前key是否存在，不存在，直接返回空值nil，解锁成功
-            "if (redis.call('hexists', KEYS[1], ARGV[3]) == 0) then " +
-                "return nil;" +
-            "end; " +
-             // 计数器数量-1(可重入锁)
-            "local counter = redis.call('hincrby', KEYS[1], ARGV[3], -1); " +
-            // 如果计数器大于0，说明还在持有锁，重新设置过期时间
-            "if (counter > 0) then " +
-                "redis.call('pexpire', KEYS[1], ARGV[2]); " +
-                "return 0; " +
-            "else " +
-            // 否则使用del指令删除key，并发布解锁成功的消息
-                "redis.call('del', KEYS[1]); " +
-                "redis.call('publish', KEYS[2], ARGV[1]); " +
-                "return 1; "+
-            "end; " +
-            "return nil;";
+		return
+		// 判断当前key是否存在，不存在，直接返回空值nil，解锁成功
+		"if (redis.call('hexists', KEYS[1], ARGV[3]) == 0) then " + 
+			"return nil;" + 
+		"end; " +
+		// 计数器数量-1(可重入锁)
+		"local counter = redis.call('hincrby', KEYS[1], ARGV[3], -1); " +
+		// 如果计数器大于0，说明还在持有锁，重新设置过期时间
+		"if (counter > 0) then " + 
+			"redis.call('pexpire', KEYS[1], ARGV[2]); " + 
+			"return 0; " + 
+		"else " +
+		// 否则使用del指令删除key，并发布解锁成功的消息
+			"redis.call('del', KEYS[1]); " + 
+			"redis.call('publish', KEYS[2], ARGV[1]); " + 
+			"return 1; " + 
+		"end; " + 
+			"return nil;";
 	}
-	
-	protected String getLockName(long threadId) {
-        return id + ":" + threadId;
-    }
 
 }
