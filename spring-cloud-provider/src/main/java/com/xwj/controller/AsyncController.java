54aa4c104ac08ebd51d417e0f7ec3a423272b98c
xuwenjin <563210736@qq.com>
2021-02-25 16:51:34 +0800

    修改Async测试用例

--- spring-cloud-provider/src/main/java/com/xwj/controller/AsyncController.java
+++ spring-cloud-provider/src/main/java/com/xwj/controller/AsyncController.java
@@ -1,32 +1,31 @@
 package com.xwj.controller;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.xwj.service.AsyncService;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * 异步测试
  * 
- * 
  * 如下方式会使@Async失效：
  * 
- * 1、异步方法使用static修饰
+ * 1、异步方法使用static或者final修饰
  * 
  * 2、异步类没有使用@Component注解（或其他注解）导致spring无法扫描到异步类
  * 
- * 3、异步方法不能与被调用的异步方法在同一个类中
+ * 3、类中需要使用@Autowired或@Resource等注解自动注入，不能自己手动new对象
  * 
- * 4、类中需要使用@Autowired或@Resource等注解自动注入，不能自己手动new对象
+ * 4、异步方法不能与被调用的异步方法在同一个类中
  * 
  * 5、如果使用SpringBoot框架必须在启动类中增加@EnableAsync注解
  * 
  */
-@Slf4j
 @RestController
 @RequestMapping("async")
 public class AsyncController {
@@ -36,10 +35,22 @@ public class AsyncController {
 
 	@GetMapping("sendMessage")
 	public String sendMessage() {
-		asyncService.sendMessage1();
-		asyncService.sendMessage2();
-		log.info("OK");
+		asyncService.sendMessage();
+		// asyncService.sendMessage2();
+		// asyncService.sendMessage3();
+		// send();
+		System.out.println("OK");
 		return "OK";
 	}
 
+	@Async
+	private void send() {
+		System.out.println("---start");
+		try {
+			TimeUnit.MILLISECONDS.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("---end");
+	}
 }
--- spring-cloud-provider/src/main/java/com/xwj/service/AsyncService.java
+++ spring-cloud-provider/src/main/java/com/xwj/service/AsyncService.java
@@ -5,34 +5,40 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Service
 public class AsyncService {
 
-	// 发送提醒短信 1
 	@Async
-	public void sendMessage1() {
-		log.info("发送短信方法---- 1   执行开始");
-		try {
-			TimeUnit.MILLISECONDS.sleep(4000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		log.info("发送短信方法---- 1   执行结束");
-	}
-
-	// 发送提醒短信 2
-	@Async
-	public void sendMessage2() {
-		log.info("发送短信方法---- 2   执行开始");
+	public void sendMessage() {
+		System.out.println("111---start");
 		try {
 			TimeUnit.MILLISECONDS.sleep(2000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		log.info("发送短信方法---- 2   执行结束");
+		System.out.println("111---end");
+	}
+
+	@Async
+	public static void sendMessage2() {
+		System.out.println("222---start");
+		try {
+			TimeUnit.MILLISECONDS.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("222---end");
+	}
+
+	@Async
+	public final void sendMessage3() {
+		System.out.println("333---start");
+		try {
+			TimeUnit.MILLISECONDS.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("333---end");
 	}
 
 }
