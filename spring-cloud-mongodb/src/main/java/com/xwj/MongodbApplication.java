615af954bdda32c0d9045dc527b2de4ab559ef44
xuwenjin <563210736@qq.com>
2021-04-22 13:46:36 +0800

    修改注释

--- spring-cloud-mongodb/src/main/java/com/xwj/MongodbApplication.java
+++ spring-cloud-mongodb/src/main/java/com/xwj/MongodbApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 测试springboot+Mongodb
  */
-//@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class }, scanBasePackages = { "com.xwj.service", "com.xwj.dao" })
-//@SpringBootApplication(scanBasePackages = { "com.xwj.service", "com.xwj.dao" })
-// @EnableMongoRepositories
-//@EnableMongoRepositories(basePackages = { "com.xwj.dao" })
 @SpringBootApplication
 public class MongodbApplication {
 
